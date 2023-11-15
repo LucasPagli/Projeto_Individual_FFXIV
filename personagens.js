c2ca4c05a6c9348bf88ce026adfa5b968551f56f
LucasPagli <lucas.dsilva@sptech.school>
2023-11-15 20:15:05 -0300

    Função de troca de informação de personagens feita

---
+++ personagens.js
@@ -0,0 +1,96 @@
+
+    function SlideRemove() {
+      ch.classList.remove("slide");
+      ci.classList.remove("slide_intro");
+      bp.classList.remove("slide_big");
+    }
+
+    function WoL() {
+
+      if (ch.className === "char_nome") {
+        ch.classList.add("slide");
+        ci.classList.add("slide_intro");
+        bp.classList.add("slide_big");
+        setTimeout(SlideRemove, 3000)
+      }
+
+      nome_hero.innerHTML = `Warrior of Light`;
+
+      intro_hero.innerHTML = `<span>O Guerreiro da Luz, também conhecido como Aventureiro, é o personagem criado pelo jogador em Final Fantasy XIV que atua como protagonista. Warrior of Light é um termo recorrente na série Final Fantasy desde o Final Fantasy original, referindo-se ao personagem do jogador.
+
+O personagem do jogador é um aventureiro em Eorzea durante a Sétima Era Umbral que se junta a uma das três Grandes Companhias e se torna um membro-chave dos Scions of the Seventh Dawn. O Guerreiro da Luz pode ser uma das oito raças principais de Eorzea: Hyur, Miqo'te, Roegadyn, Lalafell, Elezen, Au Ra, Viera ou Hrothgar. Nas cenas em CG e na maioria dos trailers do jogo, um jovem Midlander Hyur representa o Guerreiro da Luz.</span>`;
+
+      img_hero.innerHTML = `<img src="imagens/WoL_big.png">`;
+    }
+
+    function Thancred() {
+
+      if (ch.className === "char_nome") {
+        ch.classList.add("slide");
+        ci.classList.add("slide_intro");
+        bp.classList.add("slide_big");
+        setTimeout(SlideRemove, 3000)
+      }
+
+      nome_hero.innerHTML = `Thancred Waters`;
+
+      intro_hero.innerHTML = `<span>Classe: Gunbreaker (Tank) <br> Thancred é um homem inteligente, de fala mansa e que gosta de mulheres, embora fique nervoso quando Minfilia Warde aponta alguns dos elementos mais sombrios de seu passado, mesmo como um elogio. Thancred é dedicado a Minfilia e a vê como uma amiga próxima, semelhante a uma irmã. Após a Calamidade, Thancred tornou-se um workaholic ao ponto da exaustão, tentando ocupar o lugar de Louisoix Leveilleur.
+
+Depois de saber que Minfilia havia se tornado a Palavra da Mãe, o temperamento de Thancred muda, tornando-se menos alegre e mais estóico e endurecido, embora ele ainda mantenha seu humor seco. Apesar disso, Thancred aceitou o destino de Minfilia.</span>`;
+
+      img_hero.innerHTML = `<img src="imagens/WoL_big.png">`;
+    }
+
+    function Yshtola() {
+
+    if (ch.className === "char_nome") {
+      ch.classList.add("slide");
+      ci.classList.add("slide_intro");
+      bp.classList.add("slide_big");
+      setTimeout(SlideRemove, 3000)
+    }
+
+    nome_hero.innerHTML = `Y'shtola Rhul`;
+
+    intro_hero.innerHTML = `<span>Classe: Black Mage (DPS) <br> Y'shtola é apresentada como "Conjuradora Cultuada", um apelido que reflete sua compostura calma. Ela é uma voz da razão taciturna e bem falada, mesmo ao lado de seus pares igualmente acadêmicos entre os Scions of the Seventh Dawn, fornecendo uma visão nítida e relevante que dificilmente se desvia do assunto em questão. Isso contrasta com Thancred Waters, que frequentemente lança frases carismáticas e extravagantes para ser desviada por seu estoicismo.
+
+    A ousadia de Y'shtola surge quando sua paciência é testada ou como contra-ataque aos comentários ousados, irrelevantes ou conflitantes de outras pessoas. Ela deriva parte dessa audácia de seu antigo mestre, Matoya, os dois trocando piadas e insultos sutis. Apesar disso, eles se preocupam profundamente um com o outro, já que Y'shtola tem o maior respeito por sua antiga mestre, e Y'shtola tenta evitar desrespeitá-la.</span>`;
+
+    img_hero.innerHTML = `<img src="imagens/WoL_big.png">`;
+    }
+
+    function Urianger() {
+
+      if (ch.className === "char_nome") {
+        ch.classList.add("slide");
+        ci.classList.add("slide_intro");
+        bp.classList.add("slide_big");
+        setTimeout(SlideRemove, 3000)
+      }
+
+      nome_hero.innerHTML = `Thancred`;
+
+      intro_hero.innerHTML = `<span>Thancred é um homem inteligente, de fala mansa e que gosta de mulheres, embora fique nervoso quando Minfilia Warde aponta alguns dos elementos mais sombrios de seu passado, mesmo como um elogio. Thancred é dedicado a Minfilia e a vê como uma amiga próxima, semelhante a uma irmã. Após a Calamidade, Thancred tornou-se um workaholic ao ponto da exaustão, tentando ocupar o lugar de Louisoix Leveilleur.
+
+Depois de saber que Minfilia havia se tornado a Palavra da Mãe, o temperamento de Thancred muda, tornando-se menos alegre e mais estóico e endurecido, embora ele ainda mantenha seu humor seco. Apesar disso, Thancred aceitou o destino de Minfilia.</span>`;
+
+      img_hero.innerHTML = `<img src="imagens/WoL_big.png">`;
+    }
+
+    function Alisae() {
+
+      if (ch.className === "char_nome") {
+        ch.classList.add("slide");
+        ci.classList.add("slide_intro");
+        bp.classList.add("slide_big");
+        setTimeout(SlideRemove, 3000)
+      }
+
+      nome_hero.innerHTML = `Thancred`;
+
+      intro_hero.innerHTML = `<span>Thancred é um homem inteligente, de fala mansa e que gosta de mulheres, embora fique nervoso quando Minfilia Warde aponta alguns dos elementos mais sombrios de seu passado, mesmo como um elogio. Thancred é dedicado a Minfilia e a vê como uma amiga próxima, semelhante a uma irmã. Após a Calamidade, Thancred tornou-se um workaholic ao ponto da exaustão, tentando ocupar o lugar de Louisoix Leveilleur.
+
+Depois de saber que Minfilia havia se tornado a Palavra da Mãe, o temperamento de Thancred muda, tornando-se menos alegre e mais estóico e endurecido, embora ele ainda mantenha seu humor seco. Apesar disso, Thancred aceitou o destino de Minfilia.</span>`;
+
+      img_hero.innerHTML = `<img src="imagens/WoL_big.png">`;
+    }
